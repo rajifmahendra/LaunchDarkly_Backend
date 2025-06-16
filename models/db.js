72dbc484f8b88ecbdb57d67118a26ea44d158deb
rahmaneffendi <abdulrahmaneffendi789@gmail.com>
2025-06-16 10:29:06 +0700

    use createpool instead of create connection on db.js

--- models/db.js
+++ models/db.js
@@ -1,17 +1,21 @@
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-db.connect((err) => {
+db.getConnection((err, connection) => {
     if (err) {
         console.error('❌ Gagal koneksi DB:', err.message);
     } else {
         console.log('✅ Terkoneksi ke MySQL DB');
+        connection.release(); // lepas koneksi setelah uji koneksi awal
     }
 });
 
