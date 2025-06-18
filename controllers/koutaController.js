ff54c39c4597e21ace4e65000518f06591e51081
rahmaneffendi <abdulrahmaneffendi789@gmail.com>
2025-06-18 14:30:58 +0700

    set anonymous to false

--- controllers/koutaController.js
+++ controllers/koutaController.js
@@ -37,8 +37,8 @@ exports.postQuota = (ldClient) => {
         try {
             const user = {
                 key: email || "anonymous-user",
-                email: email,
-                anonymous: true
+                email: email
+                // anonymous: true
             };
 
             await ldClient.waitForInitialization();
